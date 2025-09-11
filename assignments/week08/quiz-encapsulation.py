50959bebb8227f6191eae9a1acb8fcf809e3efbc
pattarasasi kwambee <pattarasasi.k@ku.th>
2025-09-11 15:18:38 +0700

    week8

--- assignments/week08/quiz-encapsulation.py
+++ assignments/week08/quiz-encapsulation.py
@@ -7,3 +7,29 @@ A method to check if it's a square (isSquare())
 
 """
     
+    class Rectangle:
+    
+    def __init__(self,length,width):
+       self.__length = length
+       self.__width = width
+
+       def getArea(self):
+           return self.__length * self.__width
+       
+       def getperimter(self):
+           return 2 * (self.__length + self.width)
+       
+       def isSduare(self):
+           if self.length == self.__width:
+              return True
+           else:
+               return False
+           
+x = Rectangle(10,5)
+print(f"Area of x = {x.getArea()}")
+print("Perimeter of x = {x.getPerimeter()}")
+
+if x.isSquare:
+    print("x is square")
+else:
+    print("x is not square")
